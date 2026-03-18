454ac3422a5011af5871a984cfbd3c51fbb2a5a2
krisna8745 <krisna8745924951@gmail.com>
2026-03-18 23:42:23 +0530

    Update LoginPage.jsx

--- project01_Agent-main/agent/src/Login/LoginPage.jsx
+++ project01_Agent-main/agent/src/Login/LoginPage.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './LoginPage.css';
 
-const BACKEND_URL = "https://api.yox9.com"; // ⚠️ Replace with your actual backend URL
+const BACKEND_URL = "https://api.9xbet24.com"; // ⚠️ Replace with your actual backend URL
 
 const LoginPage = ({ setIsLoggedIn }) => {
   const [agentNo, setAgentNo] = useState('');
